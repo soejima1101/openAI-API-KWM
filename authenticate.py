beadf385454249756bd082d06086d93e9783d202
soejima1101 <102293103+soejima1101@users.noreply.github.com>
2023-04-20 18:45:54 +0900

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -21,25 +21,19 @@ authenticator = stauth.Authenticate(
 
 name, authentication_status, username = authenticator.login('Login', 'main')
 
-if authentication_status:
-    openai_app_main()
-    
-    # with st.sidebar:
-    #     authenticator.logout('Logout', 'main')
-    #     st.write(f'Welcome *{name}*')
-    
-    
-elif authentication_status is False:
-    st.error('Username/password is incorrect')
-# elif authentication_status is None:
-#     st.warning('Please enter your username and password')
-    
-if st.session_state["authentication_status"]:
-    with st.sidebar:
-        authenticator.logout('Logout', 'main')
-        st.write(f'Welcome *{st.session_state["name"]}*')
+try:
+    if authentication_status:
+        openai_app_main()
+        
+    # elif authentication_status is False:
+    #     st.error('Username/password is incorrect')
+        
+    if st.session_state["authentication_status"]:
+        with st.sidebar:
+            st.write(f'Welcome *{st.session_state["name"]}* !!')
+            authenticator.logout('Logout', 'main')
 
-elif st.session_state["authentication_status"] is False:
-    st.error('Username/password is incorrect')
-# elif st.session_state["authentication_status"] is None:
-#     st.warning('Please enter your username and password')
+    elif st.session_state["authentication_status"] is False:
+        st.error('Username/password is incorrect')
+except:
+    st.error("Session ended. Please refresh the screen.")
